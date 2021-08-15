4223657018e84a5bcd0603385b273deee5ee5a7c
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-15 15:18:02 -0300

    feat: add singup dto validation

--- auth-service/src/users/dto/sign-up.dto.ts
+++ auth-service/src/users/dto/sign-up.dto.ts
@@ -1,11 +1,21 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class SignUpDto {
   @IsString()
+  @IsEmail()
   @IsNotEmpty()
   email: string;
 
   @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   @IsNotEmpty()
   password: string;
 
