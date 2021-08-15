6e724b6ff183e5b233db344a39910ee610b06dd0
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-15 14:34:45 -0300

    feat: add validation pipe and class

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
--- auth-service/src/users/dto/sign-up.dto.ts
+++ auth-service/src/users/dto/sign-up.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class SignUpDto {
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsOptional()
   name: string;
 }
