cb935d4258345e75c2bc86348bec434d40a256ae
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-15 15:50:06 -0300

    feat: add mongoose module and config module

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
