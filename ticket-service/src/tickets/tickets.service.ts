7ad8849417de55e69119ef25861fb9223a2996c8
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-07 00:18:36 -0300

    feat: add ticket created publish

--- ticket-service/src/tickets/tickets.service.ts
+++ ticket-service/src/tickets/tickets.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { CreateTicketDto } from './dtos/create-ticket.dto';
 import { UpdateTicketDto } from './dtos/update-ticket.dto';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 import { Ticket } from './models/ticket.model';
 import { TicketsRepository } from './repositories/tickets.repository';
 import { TicketDocument } from './schemas/ticket.schema';
@@ -19,12 +20,19 @@ export type UpdateTicketParams = UpdateTicketDto & {
 
 @Injectable()
 export class TicketsService {
-  constructor(private readonly ticketsRepository: TicketsRepository) {}
+  constructor(
+    private readonly ticketsRepository: TicketsRepository,
+    private readonly ticketCreatedPublisher: TicketCreatedPublisher,
+  ) {}
 
   async create(createTicketDto: CreateTicketParams): Promise<Ticket> {
     const ticketDocument = await this.ticketsRepository.create(createTicketDto);
 
-    return Ticket.fromDocument(ticketDocument);
+    const ticket = Ticket.fromDocument(ticketDocument);
+
+    await this.ticketCreatedPublisher.publish(ticket);
+
+    return ticket;
   }
 
   async findById(id: string): Promise<Ticket> {
