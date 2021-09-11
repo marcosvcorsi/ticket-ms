ffbb8f768945d7bcf07d48c4935b6c73b6a605a2
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-10 22:08:21 -0300

    feat: add appmodule infra structure

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './shared/strategies/jwt.strategy';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        NATS_CLUSTER_ID: Joi.string().required(),
+        NATS_CLIENT_ID: Joi.string().required(),
+        NATS_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
+  controllers: [],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
