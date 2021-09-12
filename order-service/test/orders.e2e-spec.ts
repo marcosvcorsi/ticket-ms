60576a2a7df1345a629c63e12d9d285243c741d2
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-11 22:05:20 -0300

    chore: add setup e2e tests

--- order-service/test/orders.e2e-spec.ts
+++ order-service/test/orders.e2e-spec.ts
@@ -1,24 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as mongoose from 'mongoose';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from '../src/orders/orders.module';
 
-describe('AppController (e2e)', () => {
+describe('OrdersController (e2e)', () => {
+  let mongo: MongoMemoryServer;
+  let connection: mongoose.Connection;
+
   let app: INestApplication;
+
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        MongooseModule.forRoot(`${mongo.getUri()}tests`),
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+        }),
+        OrdersModule,
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    connection = await moduleFixture.get(getConnectionToken());
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await connection.dropDatabase();
+    await connection.close(true);
+  });
+
+  afterAll(async () => {
+    await mongo.stop();
+  });
+
+  describe('GET /orders', () => {
+    it('should return 401 when token is not provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/tickets')
+        .send();
+
+      expect(response.status).toBe(401);
+    });
   });
 });
