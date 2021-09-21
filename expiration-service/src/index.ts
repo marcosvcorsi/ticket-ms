c39c12a5a960a3c1582159d55bf23c04e3223bb3
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-20 21:57:04 -0300

    feat: add nats config index

--- expiration-service/src/index.ts
+++ expiration-service/src/index.ts
@@ -1,1 +1,35 @@
-console.log('RUN');
+import { natsClient } from "@mvctickets/common";
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+    await natsClient.connect({
+      clientId: process.env.NATS_CLIENT_ID,
+      clusterId: process.env.NATS_CLUSTER_ID,
+      url: process.env.NATS_URL
+    })
+
+    natsClient.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    
+    process.on('SIGINT', () => natsClient.client.close());
+    process.on('SIGTERM', () => natsClient.client.close());
+  } catch(error) {
+    console.error(error); 
+  }
+};
+
+start();
