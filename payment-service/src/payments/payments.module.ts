3dcd09f3b6d44e91714ef7e8bfbd06b7ec75cf0e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-25 21:45:08 -0300

    feat: add order schema to module

--- payment-service/src/payments/payments.module.ts
+++ payment-service/src/payments/payments.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Order.name,
+        useFactory: async () => {
+          const schema = OrderSchema;
+
+          schema.set('versionKey', 'version');
+          schema.pre('findOneAndUpdate', function () {
+            const update = this.getUpdate() as any;
+            if (update.version != null) {
+              delete update.version;
+            }
+            const keys = ['$set', '$setOnInsert'];
+            for (const key of keys) {
+              if (update[key] != null && update[key].version != null) {
+                delete update[key].version;
+                if (Object.keys(update[key]).length === 0) {
+                  delete update[key];
+                }
+              }
+            }
+            update.$inc = update.$inc || {};
+            update.$inc.version = 1;
+          });
+          schema.plugin(updateIfCurrentPlugin);
+
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [PaymentsController],
-  providers: [PaymentsService]
+  providers: [PaymentsService],
 })
 export class PaymentsModule {}
