dc9ee692944bac35c3990d5fe95834a060f6f392
Marcos Corsi <marcosvcorsi@gmail.com>
2021-10-02 21:27:14 -0300

    feat: add request new tickets

--- web/pages/tickets/new.js
+++ web/pages/tickets/new.js
@@ -1,8 +1,13 @@
 import { useState } from "react"
+import Router from "next/router";
+import Errors from "../../components/Errors";
+import useRequest from "../../hooks/use-request"
 
 export default function NewTicket() {
   const [title, setTitle] = useState("")
   const [price, setPrice] = useState("")
+
+  const { doRequest, errors } = useRequest();
 
   const handlePriceBlur = () => {
     const value = parseFloat(price);
@@ -14,10 +19,27 @@ export default function NewTicket() {
     setPrice(value.toFixed(2));
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const data = await doRequest({
+      url: '/api/tickets',
+      method: 'post',
+      body: {
+        title,
+        price: Number(price),
+      }
+    })
+
+    if(data) {
+      Router.push("/");
+    }
+  }
+
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="title">Title</label>
           <input 
@@ -40,7 +62,7 @@ export default function NewTicket() {
             onChange={e => setPrice(e.target.value)} 
           />
         </div>
-
+        <Errors errors={errors} />
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
