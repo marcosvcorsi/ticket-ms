43868051c3c92edda755fc622434bbfa480d65fc
Marcos Corsi <marcosvcorsi@gmail.com>
2021-10-05 21:49:15 -0300

    chore: remove nats tests project

--- nats-test/src/events/tickets/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "../publisher";
-import { Subjects } from "../subjects";
-import { TicketCreatedEvent } from "./ticket-created-event";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
